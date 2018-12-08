71f125e67a4f78739729687ed82cd06fcc6932d3
Tommy Spurlock <tspurloc@gmail.com>
2018-12-08 09:59:51 -0500

    javascript intro

---
+++ script.js
@@ -0,0 +1,42 @@
+var thomasObject = {
+    firstName: "Thomas",
+    lastName: "Spurlock",
+    age: "30",
+    likesTravel: true
+};
+console.log(thomasObject.firstName);
+console.log(thomasObject.lastName);
+console.log(thomasObject.age);
+console.log(thomasObject.likesTravel);
+
+var movieArray = ["A Goofy Movie", "Step Brothers", "Forgetting Sarah Marshall"];
+
+console.log(movieArray[0]);
+console.log(movieArray[1]);
+console.log(movieArray[2]);
+
+var williamGrades = [62, 97, 99, 85, 73, 97];
+
+console.log(williamGrades[0])
+console.log(williamGrades[5])
+
+var firstItem = williamGrades[0]
+var secondItem = williamGrades[1]
+var thirdItem = williamGrades[2]
+var fourthItem = williamGrades[3]
+var fifthItem = williamGrades[4]
+var sixthItem = williamGrades[5]
+
+var averageGrade = (firstItem + secondItem + thirdItem + fourthItem + fifthItem + sixthItem)/6
+
+console.log(averageGrade)
+
+
+var robertObject = {
+    firstName: "Robert",
+    lastName: "Downey Jr.",
+    age: "Ironman",
+    favoriteFood: "Tacos"
+};
+console.log(robertObject.favoriteFood);
+console.log(robertObject.firstName,robertObject.lastName);
